0b7bb5621179b4002fa7f62c8d2f92bd2f42b053
spectrumcity22 <rickychopra@me.com>
2025-06-02 17:34:45 +0100

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
+    <main>
+      <h1>BrandAION is live</h1>
+      <p>This is our new homepage.</p>
+    </main>
+  );
+}
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
