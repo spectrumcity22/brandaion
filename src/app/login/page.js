e0aa8e24289ae7b20cb5c433c8153ef4033c0ada
Ricky Chopra <rickychopra@Rickys-Mac-Studio.local>
2025-06-22 14:50:52 +0100

    Fix unescaped apostrophe in login page causing build error

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -129,7 +129,7 @@ export default function LoginPage() {
 
           <div className="mt-6 text-center">
             <p className="text-gray-400 text-sm">
-              Don't have an account?{' '}
+              Don&apos;t have an account?{' '}
               <Link href="/signup" className="text-blue-400 hover:text-blue-300 font-medium transition-colors">
                 Sign up
               </Link>
